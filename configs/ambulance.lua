017464025250a99a4f483f5f9d590939996ab072
GG79 <midlifegamingcrisis@gmail.com>
2024-01-30 21:53:54 -0800

    added ambulance locks

---
+++ configs/ambulance.lua
@@ -0,0 +1,76 @@
+
+
+-- staff created by GamerGirlie79_YT
+Config.DoorList['ambulance-staff'] = {
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 160.00003051758,
+    distance = 5,
+    doorType = 'door',
+    objCoords = vec3(309.133728, -597.751465, 43.433910),
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    doorLabel = 'Staff',
+    objName = 854291622,
+    fixText = false,
+}
+
+-- garageleft created by GamerGirlie79_YT
+Config.DoorList['ambulance-garageleft'] = {
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 160.00605773926,
+    distance = 5,
+    doorType = 'garage',
+    objCoords = vec3(330.134918, -561.833130, 29.775291),
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    doorLabel = 'Garage Left',
+    objName = -820650556,
+    fixText = false,
+}
+
+-- garageright created by GamerGirlie79_YT
+Config.DoorList['ambulance-garageright'] = {
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 160.00607299805,
+    distance = 5,
+    doorType = 'garage',
+    objCoords = vec3(337.277679, -564.432007, 29.775291),
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    doorLabel = 'Garage Right',
+    objName = -820650556,
+    fixText = false,
+}
+
+-- chief created by GamerGirlie79_YT
+Config.DoorList['ambulance-chief'] = {
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 340.00003051758,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(339.004974, -586.703369, 43.433910),
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    doorLabel = 'Chief',
+    objName = 854291622,
+    fixText = false,
+}
+
+-- lab created by GamerGirlie79_YT
+Config.DoorList['ambulance-lab'] = {
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 340.00003051758,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(307.118195, -569.568970, 43.433910),
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    doorLabel = 'Lab',
+    objName = 854291622,
+    fixText = false,
+}
