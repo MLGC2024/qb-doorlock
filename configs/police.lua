7cf7625bf0d3051913456abd6a48933c5afd4834
GG79 <midlifegamingcrisis@gmail.com>
2024-01-30 21:42:50 -0800

    added police doorlocks

---
+++ configs/police.lua
@@ -0,0 +1,330 @@
+
+
+-- police created by GamerGirlie79_YT
+Config.DoorList['police-entrright'] = {
+    doorLabel = 'Entrance Right',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -96679321,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(440.520081, -986.233459, 30.823193),
+    objYaw = 180.00001525879,
+}
+
+-- policeleft created by GamerGirlie79_YT
+Config.DoorList['police-entrleft'] = {
+    doorLabel = 'Entrance Left',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1406685646,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(440.520081, -977.601074, 30.823193),
+    objYaw = 0.0,
+}
+
+-- policesideright created by GamerGirlie79_YT
+Config.DoorList['police-sideright'] = {
+    doors = {
+        {objName = -1547307588, objYaw = 180.00001525879, objCoords = vec3(443.061768, -998.746216, 30.815304)},
+        {objName = -1547307588, objYaw = 0.0, objCoords = vec3(440.739197, -998.746216, 30.815304)}
+    },
+    pickable = true,
+    doorLabel = 'Side Right',
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+    locked = true,
+    authorizedJobs = { ['police'] = 0 },
+}
+
+-- policesideleft created by GamerGirlie79_YT
+Config.DoorList['police-sideleft'] = {
+    doors = {
+        {objName = -1547307588, objYaw = 0.0, objCoords = vec3(455.886169, -972.254272, 30.815308)},
+        {objName = -1547307588, objYaw = 180.00001525879, objCoords = vec3(458.208740, -972.254272, 30.815308)}
+    },
+    pickable = true,
+    doorLabel = 'Side Left',
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+    locked = true,
+    authorizedJobs = { ['police'] = 0 },
+}
+
+-- policebaseright created by GamerGirlie79_YT
+Config.DoorList['police-baseright'] = {
+    doorLabel = 'Basement Right',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = 1830360419,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(464.159058, -974.665588, 26.370705),
+    objYaw = 269.79000854492,
+}
+
+-- policebaseleft created by GamerGirlie79_YT
+Config.DoorList['police-baseleft'] = {
+    doorLabel = 'Basement Left',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = 1830360419,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(464.156555, -997.509277, 26.370705),
+    objYaw = 89.870010375977,
+}
+
+-- policegarageleft created by GamerGirlie79_YT
+Config.DoorList['police-garageleft'] = {
+    doorLabel = 'Garage Left',
+    doorType = 'garage',
+    doorRate = 1.0,
+    distance = 5,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = 2130672747,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(452.300507, -1000.771667, 26.696609),
+    objYaw = 0.0,
+}
+
+-- policegarageright created by GamerGirlie79_YT
+Config.DoorList['police-garageright'] = {
+    doorLabel = 'Garage Right',
+    doorType = 'garage',
+    doorRate = 1.0,
+    distance = 5,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = 2130672747,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(431.411926, -1000.771667, 26.696609),
+    objYaw = 0.0,
+}
+
+-- policebackentrance created by GamerGirlie79_YT
+Config.DoorList['police-backentrance'] = {
+    doors = {
+        {objName = -692649124, objYaw = 180.00001525879, objCoords = vec3(469.774261, -1014.406006, 26.483816)},
+        {objName = -692649124, objYaw = 0.0, objCoords = vec3(467.368622, -1014.406006, 26.483816)}
+    },
+    pickable = true,
+    doorLabel = 'Back Entrance',
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+    locked = true,
+    authorizedJobs = { ['police'] = 0 },
+}
+
+-- policecellmain created by GamerGirlie79_YT
+Config.DoorList['police-cellmain'] = {
+    doorLabel = 'Cell Main',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(476.615692, -1008.875427, 26.480055),
+    objYaw = 270.13998413086,
+}
+
+-- policecellmain2 created by GamerGirlie79_YT
+Config.DoorList['police-cellmain2'] = {
+    doorLabel = 'Cell Main 2',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(481.008362, -1004.117981, 26.480055),
+    objYaw = 180.00001525879,
+}
+
+-- policecell1 created by GamerGirlie79_YT
+Config.DoorList['police-cell1'] = {
+    doorLabel = 'Cell 1',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(484.176422, -1007.734375, 26.480055),
+    objYaw = 180.00001525879,
+}
+
+-- policecell2 created by GamerGirlie79_YT
+Config.DoorList['police-cell2'] = {
+    doorLabel = 'Cell 2',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(486.913116, -1012.188660, 26.480055),
+    objYaw = 0.0,
+}
+
+-- policecell3 created by GamerGirlie79_YT
+Config.DoorList['police-cell3'] = {
+    doorLabel = 'Cell 3',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(483.912720, -1012.188660, 26.480055),
+    objYaw = 0.0,
+}
+
+-- policecell4 created by GamerGirlie79_YT
+Config.DoorList['police-cell4'] = {
+    doorLabel = 'Cell 4',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(480.912811, -1012.188660, 26.480055),
+    objYaw = 0.0,
+}
+
+-- policecell5 created by GamerGirlie79_YT
+Config.DoorList['police-cell5'] = {
+    doorLabel = 'Cell 5',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -53345114,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(477.912598, -1012.188660, 26.480055),
+    objYaw = 0.0,
+}
+
+-- policeevidence created by GamerGirlie79_YT
+Config.DoorList['police-evidence'] = {
+    doorLabel = 'Evedence Room',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 1,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -692649124,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(475.832336, -990.483948, 26.405483),
+    objYaw = 134.97177124023,
+}
+
+-- policeheli created by GamerGirlie79_YT
+Config.DoorList['police-heli'] = {
+    doorLabel = 'Helipad',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -692649124,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(464.308563, -984.528442, 43.771240),
+    objYaw = 89.999977111816,
+}
+
+-- policechief created by GamerGirlie79_YT
+Config.DoorList['police-chief'] = {
+    doorLabel = 'Police Chief',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -96679321,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(458.654327, -990.649780, 30.823193),
+    objYaw = 270.00003051758,
+}
+
+-- policearmory created by GamerGirlie79_YT
+Config.DoorList['police-armory'] = {
+    doorLabel = 'Police Armory',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -692649124,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(487.437836, -1000.189270, 30.786972),
+    objYaw = 181.28001403809,
+}
+
+-- policearmory2 created by GamerGirlie79_YT
+Config.DoorList['police-armory2'] = {
+    doorLabel = 'Police Armory 2',
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -692649124,
+    pickable = true,
+    locked = true,
+    objCoords = vec3(479.750732, -999.629028, 30.789167),
+    objYaw = 89.999977111816,
+}
+
+-- gate created by GamerGirlie79_YT
+Config.DoorList['police-gate'] = {
+    doorType = 'doublesliding',
+    doors = {
+        {objName = -1868050792, objYaw = 269.99639892578, objCoords = vec3(410.025787, -1024.225952, 29.220221)},
+        {objName = -1635161509, objYaw = 270.0, objCoords = vec3(410.025787, -1024.219971, 29.220200)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Gate',
+    distance = 5,
+    doorRate = 1.0,
+}
